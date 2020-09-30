341aa4fe89bae6037beabbf80255e8115f53c991
Nujood <shawar.nujood@gmail.com>
2020-09-30 09:26:09 +0300

    insert title into one of posts query

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,19 @@ import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
+  //connect to db
   const orm = await MikroORM.init(mikroConfig);
 
-  console.log(orm.em);
+  // run migration at first
+  await orm.getMigrator().up();
+
+  // run sql
   const post = orm.em.create(Post, { title: "This is my new post" });
 
-  // instert post into db
+  // insert post into db
   await orm.em.persistAndFlush(post);
 
-  await orm.em.nativeInsert(Post, { title: "This is my new post" });
+  // await orm.em.nativeInsert(Post, { title: "This is my new post" });
 };
 
 main().catch((err) => {
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,24 @@
 import { __production__ } from "./constants";
 import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    //.ts$/ is only for ts, that way it's for js too
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
   entities: [Post],
   dbName: "lireddit",
   type: "postgresql",
   user: "inspiron",
   password: "123",
   debug: !__production__,
-  baseDir: __dirname,
-} as const;
+} as Parameters<typeof MikroORM.init>[0];
 
-// as const fixxed the type import issue in index.ts for mikroConfig variable
+// as const fixed the type import issue in index.ts for mikroConfig variable
+
+// MikroOrm.init "fn" fixed the issue on none AutoComplete inside the exported object here after we added as const at the end.
+
+//Note: Parameters return an Array
