cd2aee7c5ac915f73dc99c24c1f2390e306b5e12
Nujood <shawar.nujood@gmail.com>
2020-10-17 17:49:18 +0300

    setting up express server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { MikroORM } from "@mikro-orm/core";
 import { __production__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from 'express'
 
 const main = async () => {
   //connect to db
@@ -10,17 +11,25 @@ const main = async () => {
   // run migration at first
   await orm.getMigrator().up();
 
+  const app = express();
+  // by using _ instead of req, we are ignoring its variable
+  app.get('/',(_, res)=>{
+    res.send("hello here")
+  })
+
+  app.listen(5000,()=>{
+    console.log('server has started on port 5000');
+    
+  } )
+
   // run sql
   // const post = orm.em.create(Post, { title: "This is my new post" });
 
   // insert post into db
   // await orm.em.persistAndFlush(post);
 
-  // await orm.em.nativeInsert(Post, { title: "This is my new post" });
-  
   //get all posts
-  const posts = await orm.em.find(Post, {});
-  console.log("_________posts__________", posts);
+  // const posts = await orm.em.find(Post, {});
 };
 
 main().catch((err) => {
